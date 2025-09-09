e819e3ac21838cd2dd864a1eafe20c65b3c5256d
admiral20 <admiral20@users.noreply.github.com>
2025-09-09 09:32:08 +0000

    deploy: a5a5fd3f4533182deca476796860ebcf2852bc2b

---
+++ assets/UseXChat-Dd0XpZGT.js
@@ -0,0 +1,14 @@
+import{d as n,c as o,a as e,o as a}from"./index-BATuKdql.js";import{_ as t}from"./_plugin-vue_export-helper-DlAUqK2U.js";const l={class:"use-x-chat-demo"},d=n({__name:"UseXChat",setup(c){return(r,s)=>(a(),o("div",l,s[0]||(s[0]=[e("h1",null,"useXChat 数据管理",-1),e("p",null,"聊天数据管理 Hook 演示",-1),e("div",{class:"demo-section"},[e("h2",null,"基础用法"),e("div",{class:"demo-content"},[e("p",null,"useXChat 是一个用于聊天数据管理的 Hook，提供了消息存储、历史记录管理等功能。"),e("div",{class:"code-example"},[e("pre",null,[e("code",null,`import { useXChat } from 'ant-design-x-vue';
+
+const { 
+  messages, 
+  addMessage, 
+  clearMessages,
+  loading 
+} = useXChat();
+
+// 添加消息
+addMessage({
+  role: 'user',
+  content: 'Hello!'
+});`)])])])],-1)])))}}),p=t(d,[["__scopeId","data-v-d4d7c147"]]);export{p as default};
--- assets/use-event-callback-DJ7X8BMs.js
+++
@@ -1,1 +0,0 @@
-import{l as t}from"./index-BhYHN2x7.js";function o(r){const e=t(r),u=t(l=>{e.value&&e.value(l)});return e.value=r,u.value}export{o as n};
